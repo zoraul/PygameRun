005b7a996be132b8c5f6f1a2dc4d81c5b25131db
Zorawar Aulakh <zor.aulakh@gmail.com>
2024-03-16 11:10:12 +0000

    Changed jump key from space to upper arrow.

--- player.py
+++ player.py
@@ -25,9 +25,12 @@ class Player(pygame.sprite.Sprite):
 		player_jump_8 = pygame.image.load('graphics/player/knight/player_jump_8.png').convert_alpha()
 		player_jump_9 = pygame.image.load('graphics/player/knight/player_jump_9.png').convert_alpha()
 		player_jump_10 = pygame.image.load('graphics/player/knight/player_jump_10.png').convert_alpha()
-
+		
 		self.player_jump = [player_jump_1, player_jump_2, player_jump_3, player_jump_4, player_jump_5, 
 					  player_jump_6, player_jump_7, player_jump_8, player_jump_9, player_jump_10]
+		
+		#TODO Future implementation of Attack feature
+		self.player_dead = pygame.image.load('graphics/player/knight/player_bend_1.png').convert_alpha()
 		
 		self.player_index = 0
 		self.image = self.player_run[self.player_index]
@@ -35,14 +38,12 @@ class Player(pygame.sprite.Sprite):
 		self.gravity = 0
 		self.player_lives = 3
 		self.player_speed = 5
-		
-
 		self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
 		self.jump_sound.set_volume(0.5)
 
 	def player_input(self):
 		keys = pygame.key.get_pressed()
-		if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+		if keys[pygame.K_UP] and self.rect.bottom >= 300:
 			self.gravity = -15
 			self.jump_sound.play()
 		if keys[pygame.K_RIGHT]:
@@ -53,9 +54,8 @@ class Player(pygame.sprite.Sprite):
 			#Tcheck if the movement is within the boundary and only then allow it
 			if (self.rect.x + self.player_speed) > self.__class__.LEFT_BONDARY:
 				self.rect.x -= self.player_speed
-		#if keys[pygame.K_DOWN]:
-			#TODO change the player to bend down
-
+		#if keys[pygame.K_SPACE]:
+			#TODO change the player in the attack animation
 
 	def apply_gravity(self):
 		self.gravity += 1
